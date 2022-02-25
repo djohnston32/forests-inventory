1fd85f15aff95c2fea87c568d1cb28104a814e9f
Devin Johnston <devinjohnston17@gmail.com>
2022-02-24 18:17:47 -0800

    Add routing with React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
-import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import ForestList from "./pages/ForestList";
+
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +10,10 @@ function App() {
       <header className="App-header">
         <p>Forests Inventory</p>
       </header>
-      <ForestList />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/forests" />} />
+        <Route path="/forests" element={<ForestList />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 test("renders forests inventory heading", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const linkElement = screen.getByText(/forests inventory/i);
   expect(linkElement).toBeInTheDocument();
 });
